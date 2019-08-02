f3c7365d0327def8a2c94b4c6e835154cce536d3
Josh <Joshua.horgen@gmail.com>
2019-08-02 13:26:58 -0700

    Add db

---
+++ volunteer_db.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "project" (
+	"id" serial NOT NULL,
+	"volunteer_ID" integer NOT NULL,
+	"project_name" varchar(255) NOT NULL,
+	CONSTRAINT "project_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "volunteer" (
+	"id" serial NOT NULL,
+	"first_name" serial(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"project_name" varchar(255) NOT NULL,
+	CONSTRAINT "volunteer_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "project" ADD CONSTRAINT "project_fk0" FOREIGN KEY ("volunteer_ID") REFERENCES "volunteer"("id");
+ALTER TABLE "project" ADD CONSTRAINT "project_fk1" FOREIGN KEY ("project_name") REFERENCES "Untitled"("project_ID");
+
+ALTER TABLE "volunteer" ADD CONSTRAINT "volunteer_fk0" FOREIGN KEY ("project_name") REFERENCES "project"("project_name");
+
